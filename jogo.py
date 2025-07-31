5aee6aed59b41ec81bba034c26edaa2ffd88412f
043ianzinn <ian.tonete@escola.pr.gov.br>
2025-07-31 12:17:48 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("##########################")
+print("# jogo de adivinhação    #")
+print("#  ianzinho moedas       #")
+print("##########################")
+print("#      🐝🐻👨🏿‍🏫🐒🇷🇺        #")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
