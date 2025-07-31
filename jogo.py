1d574ba5f95c0b63f8e6432fa0b5622fa37e80d1
043ianzinn <ian.tonete@escola.pr.gov.br>
2025-07-31 14:50:02 +0000

    fim do game

--- jogo.py
+++ jogo.py
@@ -3,8 +3,23 @@ print("##########################")
 print("# jogo de adivinhação    #")
 print("#  ianzinho moedas       #")
 print("##########################")
-print("#      🐝🐻👨🏿‍🏫🐒🇷🇺        #")
+print("#      🐝🐻👨🏿‍🏫🐒🇷        #")
 
 numeroSecreto= random.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+
+print("Qual os níveis de dificuldade? ")
+print("(1)- Facil (2)- Médio (3)- Dificil ")
+
+nivel = int(input("Escolha um nível:"))
+
+if(nivel == 1):
+     print("KKKKKK MUITO FACIL")
+     totalTentativas = 20
+elif(nivel == 2):
+     print("É DA PRA BRINCAR UM POUCO")   
+     totalTentativas = 9  
+elif(nivel == 3):
+     print("TEM CORAJEM")
+     totalTentarivas = 3
