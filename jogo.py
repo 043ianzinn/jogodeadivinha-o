8f71560af012df5ce84980e86cd34e31229b989c
043ianzinn <ian.tonete@escola.pr.gov.br>
2025-08-04 14:07:52 +0000

    aula4

--- jogo.py
+++ jogo.py
@@ -33,7 +33,22 @@ elif(nivel == 3):
           if(chute < 1 or > 100):
              print("Número invalido")
              continue
-
+ 
           acertou = chute == numeroSecreto
           maior = chute > numeroSecreto
           menor = chute < numeroSecreto
+
+          if(acertou)
+              print(f"Você acertou e fez {pontos}! ")
+              break
+          else:
+               if(maior):
+                    print("Você errou! Seu chute foi maior que o número secreto")
+               elif(menor):
+                    print("Você errou! Seu chute foi menor que o número secreto")
+
+                    pontosPerdidos = abs(numeroSecreto - chute)
+                    pontos = pontos - pontosPerdidos
+
+print("Fim de jogo ! O número era ",numeroSecreto)
+
